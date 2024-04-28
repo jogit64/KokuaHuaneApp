f6baf69483843cb88ebd1993b0872dacdfcb1038
jogit64 <usersfdc18@gmail.com>
2024-04-28 15:48:56 +0200

    JWT 1.2 cors

--- kokuahuane.py
+++ kokuahuane.py
@@ -13,7 +13,11 @@ headers = {
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
-CORS(app, resources={r"/ask": {"origins": ["https://kokua.fr", "https://www.kokua.fr"]}})
+CORS(app, supports_credentials=True, resources={
+    r"/ask": {"origins": ["https://kokua.fr", "https://www.kokua.fr"]},
+    r"/login": {"origins": ["https://kokua.fr", "https://www.kokua.fr"]}
+})
+
 
 def ask_chatgpt(prompt):
     data = {
@@ -37,19 +41,32 @@ def hello():
         return render_template('index.html', question=question, response=response)
     return render_template('index.html', question=None, response=None)
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'OPTIONS'])
 def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username == 'your_username' and password == 'your_password':
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token), 200
-    else:
-        return jsonify({"msg": "Invalid credentials"}), 401
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Origin', 'https://kokua.fr')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'POST,OPTIONS')
+        return response
 
-@app.route('/ask', methods=['POST'])
-@jwt_required()
+    if request.method == 'POST':
+        username = request.json.get('username')
+        password = request.json.get('password')
+        if username == 'johann' and password == 'johann':
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token), 200
+        else:
+            return jsonify({"msg": "Invalid credentials"}), 401
+
+
+@app.route('/ask', methods=['POST', 'OPTIONS'])  # Ajout de 'OPTIONS' ici
+@jwt_required(optional=True)  # `optional=True` permet à Flask-JWT-Extended de traiter la requête OPTIONS sans exiger un jeton.
 def ask():
+    if request.method == 'OPTIONS':
+        # Laissez Flask-CORS gérer la requête OPTIONS si vous n'avez rien de spécifique à faire.
+        return {}, 200
+    
     current_user = get_jwt_identity()
     question = request.json.get('question')
     response = ask_chatgpt(question)
