8b573d70b3cb976dc59a87f24663a877802477bd
jogit64 <usersfdc18@gmail.com>
2024-05-10 21:01:44 +0200

    start v  mood 1.9i

--- kokuahuane.py
+++ kokuahuane.py
@@ -535,7 +535,7 @@ def propose_event():
     # Vérifie si un événement clair est détecté
     if not event_detection or event_detection.strip() == "Aucun événement détecté":
         # Directement retourner un message invitant l'utilisateur à fournir plus de détails
-        return jsonify({"status": "info", "message": "Nous n'avons pas identifié d'événement spécifique. Pouvez-vous donner plus de détails pour que nous puissions mieux vous aider ?"})
+        return jsonify({"status": "info", "message": "Aucun événement spécifique n'a été identifié. Pouvez-vous donner plus de détails pour que nous puissions vous aider davantage ?"})
     else:
         # Si un événement est détecté, demandez la confirmation
         logging.debug(f"Event detected: {event_detection}")
